d602662fdf3a900c2c055524f84200075553623c
Suzie Lee <wnsuwls0707@gmail.com>
2020-08-06 17:11:23 +0900

    [node.js SHC]9강- 16강

---
+++ SHcode/syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('kasfjlakdjgflakdsjglkajsglkjasldkgjlsadkjgkjasdlgjf  fdsalfkjadslfj'.length);
---
+++ SHcode/syntax/variable.js
@@ -0,0 +1,6 @@
+var letter = 'kfla egoing sdjglkadshglksadbglas egoing kdfklasdhfjasdfkdasl;fas'
+console.log(letter);
+
+var name = 'suzie'
+var letter2 = 'egoing\n'+name+ 'lsdkajglaksglnlk\n '+name;
+console.log(letter2);
