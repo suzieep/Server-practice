bb91b432ef2545873949f600c2161881f9e8d061
Suzie Lee <wnsuwls0707@gmail.com>
2020-08-21 21:34:26 +0900

    /26

---
+++ SHcode/syntax/function2.js
@@ -0,0 +1,12 @@
+const { setupMaster } = require("cluster");
+
+console.log(Math.round(1.6)); //2 반올림
+
+function sum(first,second){ //parameter variable
+
+    return first+second;
+
+}
+console.log(sum(2,4)); //argument  입력값. 인자
+
+Math.round(1.6)
